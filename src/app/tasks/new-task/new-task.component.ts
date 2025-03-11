a571b45cf85b9073c7d270e26b7d19e36a438027
adita111 <adita1adita@yahoo.com>
2025-03-11 09:25:31 +0200

    60

--- src/app/tasks/new-task/new-task.component.ts
+++ src/app/tasks/new-task/new-task.component.ts
@@ -1,13 +1,29 @@
-import { Component } from '@angular/core';
-
+import { Component, Output,EventEmitter, inject } from '@angular/core';
+import { FormsModule } from '@angular/forms';
+import { type NewTaskData } from '../task/task.model';
+import { TaskService } from '../tasks.service';
 @Component({
   selector: 'app-new-task',
   standalone: true,
-  imports: [],
+  imports: [FormsModule],
   templateUrl: './new-task.component.html',
   styleUrl: './new-task.component.css'
 })
 export class NewTaskComponent {
+@Output() cancel = new EventEmitter<void>();
+@Output() add = new EventEmitter<NewTaskData>();
+enteredTitle = '';
+enterdSummary = '';
+enterdDate = '';
 
-  
+private tasksService= inject(TasksService)
+
+  onCancel(){
+this.cancel.emit();
+  }
+
+onSubmit(){
+this.add.emit({title:this.enteredTitle,summary:this.enterdSummary,date:this.enterdDate});
+}
+
 }
